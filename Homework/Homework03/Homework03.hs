f3fd3714f99c357887f8fff89e80a008916c5705
Ariady Putra <ariady.putra@gmail.com>
2023-04-15 18:55:15 +0700

    Homework03

--- Homework/Homework03/Homework03.hs
+++ Homework/Homework03/Homework03.hs
@@ -3,7 +3,23 @@
 -- returns a message accordingly. 
 -- The function has to take the hourly consumption of an electrical device, the hours of daily use, and the maximum monthly consumption allowed.
 -- (Monthly usage = consumption (kW) * hours of daily use (h) * 30 days).
+maximumAllowed :: Integer
+maximumAllowed = 270
 
+monthlyUsage :: Integer -> Integer -> Integer
+monthlyUsage kW h = kW * h * 30
+-- >>> monthlyUsage 3 3
+-- 270
+
+q1 :: Integer -> Integer -> String
+q1 kW h =
+    let usage = monthlyUsage kW h
+    in case compare usage maximumAllowed of
+        GT -> "Bigger"
+        EQ -> "Equal"
+        LT -> "Smaller"
+-- >>> q1 3 3
+-- "Equal"
 
 -- Question 2
 -- Prelude:
@@ -11,7 +27,19 @@
 -- So `show 3` will produce `"3"` and `show (3 > 2)` will produce `"True"`.
 
 -- In the previous function, return the excess/savings of consumption as part of the message.
-
+q2 :: Integer -> Integer -> String
+q2 kW h =
+    let usage = monthlyUsage kW h
+    in case compare usage maximumAllowed of
+        GT -> "Excess " ++ show (usage - maximumAllowed)
+        EQ -> "No excess/savings"
+        LT -> "Savings " ++ show (maximumAllowed - usage)
+-- >>> q2 1 1
+-- >>> q2 3 3
+-- >>> q2 4 4
+-- "Savings 240"
+-- "No excess/savings"
+-- "Excess 210"
 
 -- Question 3
 -- Write a function that showcases the advantages of using let expressions to split a big expression into smaller ones.
