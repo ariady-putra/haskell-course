cd0a7617ead3a975e7f59da6d48428a7b679e10f
Ariady Putra <ariady.putra@gmail.com>
2023-04-19 22:33:58 +0700

    Homework12

--- Homework/Homework12/Homework.hs
+++ Homework/Homework12/Homework.hs
@@ -3,7 +3,10 @@
 -- Question 1
 -- Write a function that takes in an integer n and returns a list of the 
 -- first n fibonacci numbers. Use the function scanl to achieve this.
-
+fibonacci :: Integer -> [Integer]
+fibonacci n = init $ foldl (\ z _ -> z ++ [sum . take 2 . reverse $ z]) [1] [1..n]
+-- >>> fibonacci 8
+-- [1,1,2,3,5,8,13,21]
 
 -- Question 2
 -- Create a cabal project with an app/ folder that contains Main.hs and a lib/
@@ -11,4 +14,7 @@
 -- that takes an integer and returns True if it is prime and False if it is not.
 -- Then in Main.hs let the program ask the user for a number and let him know
 -- wheather it is prime or not by using the module defined in Libraries.hs.
-
+prime :: Integer -> Bool
+prime n = n > 1 && all ((0/=) . (mod n)) [2..(div n 2)]
+-- >>> filter prime [-100..100]
+-- [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97]
