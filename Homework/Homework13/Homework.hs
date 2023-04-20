cc91c0b1bafefcc5f704cbb4d70755dcd20c5b5d
Ariady Putra <ariady.putra@gmail.com>
2023-04-20 20:17:16 +0700

    Homework13

--- Homework/Homework13/Homework.hs
+++ Homework/Homework13/Homework.hs
@@ -12,6 +12,21 @@ Hint: since Haskell byte strings are a list of Word8 (base 256 integers) (see th
 -}
 
 byteStringToInteger :: BS.ByteString -> Integer
+byteStringToInteger bs =
+    (BS.foldl
+        (\ b w p ->
+            b (p+1)
+            +
+            256^p * toInteger w
+        ) id bs
+    ) 0
+    -
+    (fromIntegral . BS.length) bs
+
+q1 = do
+    bs <- BS.getLine
+    putStrLn . show $ BS.foldr ((:) . toInteger) [] bs
+    putStrLn . show $ byteStringToInteger bs
 
 {-
 Create a function that converts Integers into the binary representation using a base 256 conversion.
@@ -20,9 +35,27 @@ You can use the abs function to make all inputs positive.
 
 Example: we can convert 3000000 in base 10 to base 256 as 
 
-        [((300000 `div` 256) `div` 256) `mod` 256,(300000 `div` 256) `mod` 256, 300000 `mod` 256]. 
+        [ ((300000 `div` 256) `div` 256) `mod` 256
+        , (300000 `div` 256) `mod` 256
+        , 300000 `mod` 256
+        ]. 
 
 Hint: since Haskell byte strings are a list of Word8 (base 256 integers) (see the toEnum :: Int -> Word8). 
 -}
 
 integerToByteString :: Integer -> BS.ByteString
+integerToByteString = pack . wordList . fromInteger
+
+wordList i -- helper function
+    | i > 0     = wordList (i `div` 256) ++ [toEnum $ i `mod` 256]
+    | otherwise = []
+
+q2 =
+    [ ((300000 `div` 256) `div` 256) `mod` 256
+    , (300000 `div` 256) `mod` 256
+    , 300000 `mod` 256
+    ]
+-- >>> wordList 300000 :: [Int]
+-- >>> q2
+-- [4,147,224]
+-- [4,147,224]
